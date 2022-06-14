504929270eeeb368251f6c65b7c193d370b146e8
Matheus_Araújo <matheusc2005@hotmail.com>
2022-06-14 14:05:48 -0300

    feat: add services to return allRentsUser

--- src/controllers/rents/listAllRentsUser.controller.ts
+++ src/controllers/rents/listAllRentsUser.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import listAllRentsUserService from "../../services/rents/listAllRentsUser.service";
 
 const listAllRentsUserController = async (req: Request, res: Response) => {
-  const listRent = await listAllRentsUserService();
+  const listRent = await listAllRentsUserService(req);
 
   return res.status(200).json(listRent);
 };
