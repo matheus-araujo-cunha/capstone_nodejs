76659d6462b7a08293758ae720af8f1c4db3b98d
Matheus_Araújo <matheusc2005@hotmail.com>
2022-06-14 14:49:53 -0300

    feat: add configs to deploy project

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,20 +5,23 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-// const TestEnv = new DataSource({
-//   type: "sqlite",
-//   database: "../dbTest.sqlite",
-//   synchronize: true,
-//   entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
-// });
+const TestEnv = new DataSource({
+  type: "sqlite",
+  database: "../dbTest.sqlite",
+  synchronize: true,
+  entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
+});
 
 const DevEnv = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
   logging: false,
   entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
   migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
 });
 
-// export default process.env.NODE_ENV === "test" ? TestEnv : DevEnv;
-export default DevEnv;
+export default process.env.NODE_ENV === "test" ? TestEnv : DevEnv;
