e9bcfd9e2bb2539c7e84ced46b16d774e2841736
Matheus_Araújo <matheusc2005@hotmail.com>
2022-06-22 13:09:44 -0300

    deleting migrations

--- src/migrations/1655830943049-initialCommit.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class initialCommit1655830943049 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
