72ae919316b921c9ccb4fa7cae8e1104047884a1
Matheus_Araújo <matheusc2005@hotmail.com>
2022-06-22 11:23:53 -0300

    docs: adding docs to users and items

--- src/services/users/userUpdate.service.ts
+++ src/services/users/userUpdate.service.ts
@@ -1,22 +1,19 @@
 import { User } from "../../entities/User";
 import userRepository from "../../repositories/users/user.repositorie";
 import { Request } from "express";
-
-
+import { serializedCreateUserSchema } from "../../schemas/users/createUser.schema";
 
 const userUpdateService = async ({
-    params,
-    body,
-  }: Request): Promise<Partial<User>> => {
-    const {id}= params
+  params,
+  body,
+}: Request): Promise<Partial<User>> => {
+  const { id } = params;
 
-    await userRepository.update(id, body as Partial<User>);
-    
-    const user = await userRepository.retrieve({userUuid:id}) as User
+  await userRepository.update(id, body as Partial<User>);
 
-    
+  const user = (await userRepository.retrieve({ userUuid: id })) as User;
 
-    return user
-  };
+  return serializedCreateUserSchema.validate(user, { stripUnknown: true });
+};
 
-  export default userUpdateService
+export default userUpdateService;
